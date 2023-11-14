286d618cc91a00a5d86f89af1e6ea4bbf613f589
Oleksandr-D <dolbnja9@gmail.com>
2023-11-14 19:18:51 +0200

    added full project

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+  public password: string = '';
+  public passwordLevel: string = "Enter more than 8 characters";
+  public firstSectionColor: string = 'rgb(143, 143, 143)';
+  public secondSectionColor: string = 'rgb(143, 143, 143)';
+  public thirdSectionColor: string = 'rgb(143, 143, 143)';
+
+  updatePasswordStrength(): void {
+    if (this.password.length > 0 && this.password.length < 8) {
+      this.firstSectionColor = 'rgb(209, 5, 5)';
+      this.secondSectionColor = 'rgb(209, 5, 5)';
+      this.thirdSectionColor = 'rgb(209, 5, 5)';
+      this.passwordLevel = 'Your password is...';
+    } else {
+      this.firstSectionColor = 'rgb(143, 143, 143)';
+      this.secondSectionColor = 'rgb(143, 143, 143)';
+      this.thirdSectionColor = 'rgb(143, 143, 143)';
+    }
+
+    if (this.password.length >= 8 && (this.isOnlyLetters() || this.isOnlyDigits() || this.isOnlySymbols())) {
+      this.firstSectionColor = 'rgb(209, 5, 5)';
+      this.secondSectionColor = 'rgb(143, 143, 143)';
+      this.thirdSectionColor = 'rgb(143, 143, 143)';
+      this.passwordLevel = 'Easy';
+    } else {
+      this.passwordLevel = 'Your password is...';
+    }
+
+    if (this.password.length >= 8) {
+      if (this.isCombinationOfLettersAndSymbols() || this.isCombinationOfLettersAndDigits() || this.isCombinationOfDigitsAndSymbols()) {
+        this.firstSectionColor = 'rgb(243, 235, 2)';
+        this.secondSectionColor = 'rgb(243, 235, 2)';
+        this.thirdSectionColor = 'rgb(143, 143, 143)';
+        this.passwordLevel = 'Medium';
+      }}
+
+    if (this.password.length > 8){
+      if (this.hasAllTypes()) {
+        this.firstSectionColor = 'rgb(18, 176, 4';
+        this.secondSectionColor = 'rgb(18, 176, 4';
+        this.thirdSectionColor = 'rgb(18, 176, 4';
+        this.passwordLevel = 'Strong';
+      } 
+    }
+  }
+ 
+  private isOnlyLetters(): boolean {
+    return /^[a-zA-Z]+$/.test(this.password);
+  }
+
+  private isOnlyDigits(): boolean {
+    return /^[0-9]+$/.test(this.password);
+  }
+
+  private isOnlySymbols(): boolean {
+    return /^[^\w\s\d]+$/.test(this.password);
+  }
+
+  private isCombinationOfLettersAndSymbols(): boolean {
+    return /[a-zA-Z]+[^a-zA-Z\d\s]+|[^a-zA-Z\d\s]+[a-zA-Z]+/.test(this.password);
+  }
+
+  private isCombinationOfLettersAndDigits(): boolean {
+    return /[a-zA-Z]+\d+|\d+[a-zA-Z]+/.test(this.password);
+  }
+  
+  private isCombinationOfDigitsAndSymbols(): boolean {
+    return /\d+[^a-zA-Z\d\s]|[^a-zA-Z\d\s]+\d+/.test(this.password);
+  }
+
+  private hasAllTypes(): boolean {
+    const has = (regex: RegExp) => regex.test(this.password);
+    return has(/[a-zA-Z]/) && has(/\d/) && has(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/);
+  }
+}
